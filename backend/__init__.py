5add1da450d2561243df9889f04c6e61996fe382
constantinax <constantina909@gmail.com>
2026-02-12 23:34:13 +0200

    Create __init__.py

---
+++ backend/__init__.py
@@ -0,0 +1,7 @@
+"""Package initializer for emotibit_backend.
+
+Having this file allows relative imports (e.g., `from . import models`) to resolve.
+"""
+from . import models
+
+__all__ = ["models"]
